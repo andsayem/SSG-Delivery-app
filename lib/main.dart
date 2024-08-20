c2c75603665210e2faa12f41c159ad65aa6c8029
andsayem <andsayem@gmail.com>
2024-08-20 10:52:33 +0600

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -18,44 +20,101 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Sales Automation'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String _connectionStatus = 'Unknown';
+  final Connectivity _connectivity = Connectivity();
+
+  @override
   void initState() {
     super.initState();
-    _launchURL('https://ssforce.ssgbd.com/otp-check');
+    _checkConnectivity();
   }
 
-  _launchURL(base_url) async {
-    final Uri url = Uri.parse(base_url);
-    if (!await await launchUrl(url, mode: LaunchMode.inAppWebView)) {
-      throw Exception('Could not launch  ');
+  Future<void> _checkConnectivity() async {
+    // ConnectivityResult result;
+    // try {
+    //   result = (await _connectivity.checkConnectivity()) as ConnectivityResult;
+    // } catch (e) {
+    //   result = ConnectivityResult.none;
+    // }
+
+    // if (result != ConnectivityResult.none) {
+    //   final bool hasInternet = await _checkInternetAccess();
+    //   if (hasInternet) {
+      //  _connectionStatus = 'Connected to Internet';
+        _launchURL('https://ssforce.ssgbd.com/otp-check');
+    //   } else {
+    //     _connectionStatus = 'No Internet Access';
+    //     _showNoConnectionDialog();
+    //   }
+    // } else {
+    //   setState(() {
+    //     _connectionStatus = 'No Internet Connection';
+    //     _showNoConnectionDialog();
+    //   });
+    // }
+  }
+
+  Future<bool> _checkInternetAccess() async {
+    try {
+      final response = await http.get(Uri.parse('https://www.google.com'));
+      if (response.statusCode == 200) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      return false;
     }
+  }
+
+  _launchURL(String baseUrl) async {
+    final Uri url = Uri.parse(baseUrl);
+    if (!await launchUrl(url, mode: LaunchMode.inAppWebView)) {
+      throw Exception('Could not launch URL');
+    }
+  }
+
+  void _showNoConnectionDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('No Internet Connection'),
+          content: Text('Please check your internet connection and try again.'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-      SystemChrome.setSystemUIOverlayStyle(
+    SystemChrome.setSystemUIOverlayStyle(
       SystemUiOverlayStyle(
-        systemNavigationBarColor:
-            HexColor("#d257aa"), // navigation bar color
-        statusBarColor:
-            Theme.of(context).colorScheme.background, // status bar color
+        systemNavigationBarColor: HexColor("#d257aa"), // navigation bar color
+        statusBarColor: Theme.of(context).colorScheme.background, // status bar color
         statusBarIconBrightness: Brightness.light, // status bar icons' color
-        systemNavigationBarIconBrightness:
-            Brightness.light, //navigation bar icons' color
+        systemNavigationBarIconBrightness: Brightness.light, // navigation bar icons' color
       ),
     );
     return Scaffold(
@@ -63,9 +122,8 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: HexColor("#d257aa"),
         centerTitle: true,
         title: Text(
-          widget.title,
-          style:
-              const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          'Sales Automation',
+          style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
       ),
       body: Center(
@@ -73,22 +131,25 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             InkWell(
-              onTap: () => _launchURL('https://ssforce.ssgbd.com/otp-check'),
+              onTap: () => _checkConnectivity(),
               child: Column(
                 children: [
-                  Container(
-                    child: Image.asset(
-                      "assets/images/logo.png",
-                      height: 100,
-                    ),
+                  Image.asset(
+                    "assets/images/logo.png",
+                    height: 100,
                   ),
-                  Icon( 
-                      Icons.arrow_forward_ios , color:HexColor("#d257aa") ,
+                  Icon(
+                    Icons.arrow_forward_ios,
+                    color: HexColor("#d257aa"),
                   )
                 ],
               ),
             ),
-           
+            SizedBox(height: 20),
+            // Text(
+            //   'Connection Status: $_connectionStatus',
+            //   style: TextStyle(fontSize: 18),
+            // ),
           ],
         ),
       ),
